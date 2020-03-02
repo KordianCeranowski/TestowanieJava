ebcb747c9736ba39a55b1ba0bf46a6181bfa77d1
KordianCeranowski <KordianCeranowski@users.noreply.github.com>
2020-03-02 17:53:41 +0100

    Test day 12 working

--- stepikLab3/src/main/java/ChristmasSong.java
+++ stepikLab3/src/main/java/ChristmasSong.java
@@ -1,10 +1,22 @@
+import java.util.Collections;
+
 class ChristmasSong {
 
+    static String[] numbersOfDays = {"first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"};
+    static String[] presents = {"twelve Drummers Drumming, ", "eleven Pipers Piping, ", "ten Lords-a-Leaping, ", "nine Ladies Dancing, ", "eight Maids-a-Milking, ", "seven Swans-a-Swimming, ", "six Geese-a-Laying, ", "five Gold Rings, ", "four Calling Birds, ", "three French Hens, ", "two Turtle Doves, and ", "a Partridge in a Pear Tree."};
+
     static String sing(int day){
-        if (day == 1)
-            return "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.";
-        else if (day == 2)
-            return "On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree." ;
-        else return "";
+        if(day <= 0 && day > 12){
+            throw new IllegalArgumentException();
+        }
+
+        String song = "On the " + numbersOfDays[day - 1] + " day of Christmas my true love gave to me: ";
+        int presentsStartAt = 12 - day;
+
+        for(int i = presentsStartAt; i < 12; i++){
+            song += presents[i];
+        }
+
+        return song;
     }
 }
