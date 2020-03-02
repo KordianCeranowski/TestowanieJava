4cb27f8e528f1d8087facf2f735fd9bf7d35eecc
KordianCeranowski <KordianCeranowski@users.noreply.github.com>
2020-03-02 17:37:21 +0100

    Test day 2 working

--- stepikLab3/src/main/java/ChristmasSong.java
+++ stepikLab3/src/main/java/ChristmasSong.java
@@ -3,6 +3,8 @@ class ChristmasSong {
     static String sing(int day){
         if (day == 1)
             return "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.";
+        else if (day == 2)
+            return "On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree." ;
         else return "";
     }
 }
