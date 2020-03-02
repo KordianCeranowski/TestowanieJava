d745c8e66553dde5714d61f19e6604f069f8c98b
KordianCeranowski <KordianCeranowski@users.noreply.github.com>
2020-03-02 16:00:23 +0100

    Printing I

--- stepikLab3/src/main/java/RomanNumeral.java
+++ stepikLab3/src/main/java/RomanNumeral.java
@@ -1,7 +1,20 @@
 public class RomanNumeral {
 
+    String roman;
+    int arabic;
+
     public RomanNumeral(int i) {
+        roman = "";
+        arabic = i;
+        translate();
     }
 
-    public String getRomanNumeral(){return "";}
+    private void translate(){
+        while(arabic > 0 && arabic < 4){
+            arabic--;
+            roman = roman + "I";
+        }
+    }
+
+    public String getRomanNumeral(){return roman;}
 }
