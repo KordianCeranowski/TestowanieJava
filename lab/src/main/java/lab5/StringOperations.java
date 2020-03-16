17965b0a53e89b4149170035600fa8eabd29b77d
KordianCeranowski <KordianCeranowski@users.noreply.github.com>
2020-03-16 14:46:37 +0100

    Cleanup

--- lab/src/main/java/lab5/StringOperations.java
+++ lab/src/main/java/lab5/StringOperations.java
@@ -3,15 +3,17 @@ package lab5;
 import java.util.ArrayList;
 import java.util.List;
 
-public class StringOperations {
+class StringOperations {
 
     String reverse(String a) {
         List<String> tempArray = new ArrayList<>(a.length());
-        for (int i = 0; i < a.length(); i++) {
+        for (int i = 0; i < a.length(); i++)
+        {
             tempArray.add(a.substring(i, i + 1));
         }
         StringBuilder reversedString = new StringBuilder(a.length());
-        for (int i = tempArray.size() - 1; i >= 0; i--) {
+        for (int i = tempArray.size() - 1; i >= 0; i--)
+        {
             reversedString.append(tempArray.get(i));
         }
         return reversedString.toString();
@@ -19,10 +21,12 @@ public class StringOperations {
 
     String concat(String a, String b) {
         String retVal = null;
-        if (a != null && a.length() > 0) {
+        if (a != null && a.length() > 0)
+        {
             StringBuilder sb = new StringBuilder();
             sb.append(a);
-            if (b != null) {
+            if (b != null)
+            {
                 sb.append(b);
             }
             retVal = sb.toString();
@@ -31,13 +35,14 @@ public class StringOperations {
     }
 
     boolean isPalindrome(String a) {
-        if(a.length() <= 1){
+        if(a.length() <= 1)
+        {
             return true;
         }
-        if(!(a.charAt(1) + "").equals(a.charAt(a.length() - 1) + "")){
+        if(!(a.charAt(0) + "").equals(a.charAt(a.length() - 1) + ""))
+        {
             return false;
         }
-        return isPalindrome(a.substring(1, a.length() - 2));
+        return isPalindrome(a.substring(1, a.length() - 1));
     }
-
 }
